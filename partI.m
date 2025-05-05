30bddfae95978f21c4640daa717e40c0cc8e285c
anish <anish@localhost>
2025-05-05 18:26:22 +1000

    Created code that initialises data and builds the A matrix

--- partI.m
+++ partI.m
@@ -11,3 +11,38 @@
 %Mask is a binary array that differentiates the MRI area of interest (brain), and unneeded noise (background).
 
 
+% Template for MXB201 Project Part I.
+
+%% Initialisation
+clear
+load partI.mat
+
+[X,Y,num_dirs] = size(S);
+assert(isequal(size(g), [num_dirs 3]));
+
+% These arrays will be be filled in during the main loop below
+MD  = nan(X, Y);    % mean diffusion
+FA  = nan(X, Y);    % fractional anistropy
+PDD = nan(X, Y, 3); % principal diffusion direction
+
+%Any other initialisation needed here
+A =[g(:,1).^2,g(:,2).^2 , g(:,3).^2 , 2*g(:,1).*g(:,2),2*g(:,1).*g(:,3),2*g(:,2).*g(:,3)]; %initialises A matrix for rearranged eq.
+
+%% Compute the diffusion tensor for each pixel
+for x = 1:X
+    for y = 1:Y
+
+        % If not within the mask, skip the pixel
+        if ~mask(x, y), continue; end
+
+        % Handling bad data
+        
+        % Solving least squares problem
+        % Forming diffusion tensor
+        % Finding eigenvalues and eigenvectors
+        % Calculating MD, FA and PDD
+
+    end
+end
+
+%% Plot mean diffusivity, fractional anisotropy and principal diffusion direction maps
