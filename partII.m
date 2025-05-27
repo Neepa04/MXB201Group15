b02f14da126f931202ac91be1eee980376bd07e2
ren0404 <bulacanwarren04@gmail.com>
2025-05-27 15:17:22 +1000

    Removed a few test sections and improved parts of code. Code was developed such that two different samples for the moustache detector is being used, namely the entire face photo database as well as the unique 35 faces in the database.Code was developed for the unique faces to display a colour coded "Yes" or "No" indicating moustache detector results for all 35 faces. The faces labelled as "Yes" were then plotted for better visualisation for the user and assist on determining detector accuracy.

--- partII.m
+++ partII.m
@@ -64,24 +64,16 @@ for i = 1:N
     c_vectors(:,i) = LHS \ RHS;
 end
 
-%% TEST
-recon_face = S*c_vectors(:,30);
-
-figure
-recon_face_vis = reshape(uint8(recon_face), rows, cols);
-imshow(recon_face_vis, 'InitialMagnification', 'Fit')
-title('TEST: Recon face')
-
 %% Demonstrate rudimentary moustache detector
 
 % The 13th column of the eigenface space corresponds to the moustache characteristic
 % Therefore, the 13th row of the coordinate vector determines the **moustache level** apparent in the photo
 % Below moustache level = moustache undetected,   Equal or above moustache level = moustache detected
 
-% The sample will be the entire face database given (all columns of A i.e 1000 photos)
+% The sample will be the entire face database given (all columns of A i.e. 1000 photos)
 
 % Isolate columns of A corresponding to faces with a moustache
-moustache_level = 1900;                             % Moustache Level
+moustache_level = 1100;                             % Moustache Level
 
 mask = c_vectors(13,:) >= moustache_level;      
 moustache_faces = A(:, mask);
@@ -103,7 +95,7 @@ end
 figure
 colnum_moustacheface = find(mask);          % Column numbers of moustache detected from A
 tiledlayout(layout, layout2, 'Padding', 'Compact', 'TileSpacing', 'Compact')
-sgtitle('Sample Results of Moustache Detector')
+sgtitle('All Faces: Faces Detected Visualisation')
 
 for i = 1:moustache_faces_cols
     nexttile
@@ -116,69 +108,69 @@ end
 fprintf("There are %d faces detected with a moustache (Moustache Level = %d).\n", moustache_faces_cols, moustache_level)
 
 % From a moustache level standpoint of 1900, the detector successfully detected 29/30 images of an individual with a
-% moustache giving an accuracy of 0.97%
-
-%% TEST
-        % TEST; Visualisation of selected faces
-figure
-tiledlayout(6, 6)
-sgtitle('TEST: Visualisation of selected faces')
-
-face = A(:,1:36);
-face_vis = reshape(face, rows, cols, 36);
-
-for u = 1:36
-    nexttile
-    imagesc(face_vis(:,:, u))
-    colormap('gray')
-    axis off
-end
+% moustache giving an accuracy of 97%
 
 
+%% TEST: ALTERNATIVE
 
-        % TEST: Visualisation of each unique face
-figure
-tiledlayout(5, 7)
-sgtitle('TEST: Visualisation of each unique face')
+% Moustache detector on a sample of 35 unique faces
 
+% Matrix of unique faces only
 mask2 = 1:29:1000;
-faces = A(:,mask2);         % Matrix of unique faces only
+uniquefaces = A(:,mask2);      
 
-faces_vis = reshape(faces, rows, cols, size(faces, 2));
-
-for u = 1:size(faces,2)
-    nexttile
-    imagesc(faces_vis(:,:, u))
-    colormap('gray')
-    axis off
-end
-
-
-% Coordinate vectors of each unique face (Least Squares Solution)
-uc_vectors = zeros(47, 35);           % Initialising unique coordinate vector matrix
-
-for i = 1:35
-    LHS = S'*S;
-    RHS = S'*faces(:, i);
-    uc_vectors(:,i) = LHS \ RHS;
-end
+% Coordinate vectors of each unique face
+uc_vectors = c_vectors(:, mask2);
 
 % Isolate columns of unique faces matrix corresponding to faces with a moustache
-mask3 = uc_vectors(13,:) > 2000;
-moustache_faces2 = faces(:,mask3);
+uf_moustache_level = 1100;                               % Moustache level
 
-% Visualising unique faces with detected moustache
-moustache_faces_vis2 = reshape(moustache_faces2,rows, cols, size(moustache_faces2, 2));
+mask3 = uc_vectors(13,:) >= uf_moustache_level;
+uf_moustache_faces = uniquefaces(:,mask3);
+uf_moustache_faces_cols = size(uf_moustache_faces, 2);   % Number of detected moustache faces (columns)   
+
+
+% Visualising unique faces with a moustache (Yes or No)
+uniquefaces_vis = reshape(uniquefaces, rows, cols, size(uniquefaces, 2));
 
 figure
-tiledlayout(1, size(moustache_faces2, 2), "Padding","Compact")
-sgtitle('TEST: Visualisation of Unique Faces with Detected Moustache')
+tiledlayout(5, 7, 'Padding', 'Compact', 'TileSpacing', 'Compact')
+sgtitle('Unique Faces: Moustache Detector Results (Yes or No)')
 
-for i = 1:size(moustache_faces2, 2)
+for u = 1:size(uniquefaces,2)
     nexttile
-    imagesc(moustache_faces_vis2(:,:, i))
+    imagesc(uniquefaces_vis(:,:, u))
+    colormap('gray')
+    if mask3(u) == 1
+        title('Yes', 'Color', [0 0.5 0])
+    else
+        title('No', 'Color', [0.5 0 0])
+    end
+    axis off
+end
+
+
+% Visualising unique faces with detected moustache
+uf_moustache_faces_vis = reshape(uf_moustache_faces, rows, cols, uf_moustache_faces_cols);
+
+% Producing an approximate square tiled layout for any moustache level
+layout = round(sqrt(uf_moustache_faces_cols));
+
+if layout^2 < uf_moustache_faces_cols
+    layout2 = layout + 1;
+else
+    layout2 = layout;
+end
+
+figure
+tiledlayout(layout, layout2, "Padding","Compact")
+sgtitle('Unique Faces: Faces Detected Visualisation')
+
+for i = 1: uf_moustache_faces_cols
+    nexttile
+    imagesc(uf_moustache_faces_vis(:,:, i))
     colormap('gray')
     axis off
 end
 
-H = uc_vectors(:, [1 2 4 6 9 20]);
+fprintf("There are %d unique faces detected with a moustache (Moustache Level = %d).\n", uf_moustache_faces_cols, uf_moustache_level)
