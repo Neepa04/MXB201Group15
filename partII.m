7a8b9920ac51dbab3d194ed8aaffe236d1451fe2
ren0404 <bulacanwarren04@gmail.com>
2025-06-01 11:14:37 +1000

    Replicated code for second sample to the first sample to determine accuracy and optimal moustache level for all faces. Denoted moustache level as phi. Improved code in all sections

--- partII.m
+++ partII.m
@@ -85,10 +85,12 @@ c_vectors = S'*A;
 % Therefore, the 13th row of the coordinate vector determines the **moustache level** apparent in the photo
 % Below moustache level = moustache undetected,   Equal or above moustache level = moustache detected
 
-% The sample will be the entire face database given (all columns of A i.e. 1000 photos)
+% The detector will be tested on two samples.
+% The first sample will be the entire face database given (all columns of A i.e. 1000 photos)
 
 % Isolate columns of A corresponding to faces with a moustache
-moustache_level = 2000;                             % Moustache Level
+moustache_level = 1754;                             % Moustache Level (ϕ)
+max_level = max(c_vectors(13,:));                   % Maximum level applicable
 
 mask = c_vectors(13,:) >= moustache_level;      
 moustache_faces = A(:, mask);
@@ -123,11 +125,91 @@ for i = 1:moustache_faces_cols
     axis off
 end
 
-fprintf("There are %d faces detected with a moustache (Moustache Level = %d).\n", ...
+fprintf("All Faces: There are %d faces detected with a moustache (Moustache Level = %d).\n", ...
     moustache_faces_cols, moustache_level)
 
-% From a moustache level standpoint of 1900, the detector successfully detected 29/30 images of an individual with a
-% moustache giving an accuracy of 97%
+% ACCURACY %
+
+% The accuracy of the detector is determined by using True Positive (TP), True Negative (TN),
+% False Positive (FP), and False Negative (FN) values into the accuracy formula:
+
+% Accuracy = TP + TN / (TP + TN + FP + FN).     (EvidentlyAI, 2025)
+
+% There are 2 faces with a moustache in the sample of 35 unique faces
+% General TP, TN, FP and FN values for any moustache level
+
+% Iniitialising true moustache position
+groundtruth = zeros(size(mask));
+groundtruth([233:261 548:576]) = 1;
+groundtruth = logical(groundtruth);
+
+TP = sum((groundtruth == 1) & (mask == 1));
+TN = sum((groundtruth == 0) & (mask == 0)); 
+FP = sum((groundtruth == 0) & (mask == 1));
+FN = sum((groundtruth == 1) & (mask == 0));
+
+% Thus, 
+Accuracy = (TP + TN) / (TP + TN + FP + FN);
+fprintf("All Faces Sample Accuracy: %.2f%% \n", Accuracy * 100)
+
+% However, it is important to check the reliability of the computed accuracy as it may be undermined 
+% by the imbalance in the sample, where faces without a moustache significantly outnumber those
+% with a moustache. Since FP = 4 and FN = 22, the reliability of the accuracy is dependent on the value of
+% an F1 score using precision and recall (EvidentlyAI, 2025).
+
+Precision = TP / (TP + FP);
+Recall = TP / (TP + FN);
+
+F1 = 2 * ((Precision * Recall)) / (Precision + Recall);
+
+% The calculated value of F1 being 0.7347 is decent, meaning the accuracy calculated is deemed reliable.
+
+% Therefore, from a moustache level standpoint of 1754, the accuracy of the moustache detector on the entire
+% face database is at a precise 97.4% accuracy. *HOWEVER*, this accuracy is specific to the sample used, and
+% may not generalise to larger or differently composed datasets.
+
+
+% Optimal moustache level: Accuracy vs. Moustache Level Plot
+
+% Calculating accuracy for moustache levels of 1 to the max level
+moustache_level_axis = 1:max_level;
+
+for i = 1:max_level
+    mask = c_vectors(13,:) >= i;
+    TP = sum((groundtruth == 1) & (mask == 1));
+    TN = sum((groundtruth == 0) & (mask == 0)); 
+    FP = sum((groundtruth == 0) & (mask == 1));
+    FN = sum((groundtruth == 1) & (mask == 0));
+    Accuracy(i,:) = (TP + TN) / (TP + TN + FP + FN);
+end
+
+% Calculating lower bound and setting upper bound of optimal interval
+% Two optimal intervals were observed of the same accuracy, the largest interval values was
+% selected as it provides a greater freedom of levels.
+
+max_accuracy_vec = find(Accuracy == max(Accuracy));         % Finding vector of max accuracy
+
+% Interval of Optimal Moustache Level
+lower_bound = min(max_accuracy_vec(max_accuracy_vec >= mean(max_accuracy_vec)));                       
+upper_bound = max(max_accuracy_vec);
+
+figure
+plot(moustache_level_axis, Accuracy * 100, 'HandleVisibility', 'off')
+title('All Faces: Accuracy vs. Moustache Level')
+
+% Plotting optimal boundary lines, adding axis titles and text
+hold on
+xline(moustache_level, 'b:', 'DisplayName', ...
+    sprintf('Chosen Moustache Level (%.2f)', moustache_level));
+xline(lower_bound, '--r', 'DisplayName',  'Optimal Moustache Level Interval');
+xline(upper_bound, '--r', 'HandleVisibility',  'off');
+xlabel('Moustache Level ϕ'), ylabel('Accuracy (%)')
+text(upper_bound + 75, Accuracy(round(upper_bound)) * 100 - 3, ...
+    sprintf('Maximum Accuracy: %.2f%% \nϕ ∈ [%.2f, %.2f]', max(Accuracy)*100, lower_bound, upper_bound), ...
+    'Color', [0.5 0 0], 'FontSize', 8)
+
+legend('Show', 'Location', 'Northwest', 'FontSize', 8)
+xlim([1000 2300]), ylim([90 100])
 
 
 %% ALTERNATIVE: SMALLER SAMPLE
@@ -142,10 +224,10 @@ uniquefaces = A(:,mask2);
 c_vectors2 = c_vectors(:, mask2);
 
 % Isolate columns of unique faces matrix corresponding to faces with a moustache
-moustache_level2 = 1847.3;                                 % Moustache level
-max_level = max(c_vectors2(13,:));                       % Maximum level applicable
+moustache_level = 1847.3;                                % Moustache level (ϕ)
+max_level2 = max(c_vectors2(13,:));                       % Maximum level applicable
 
-mask3 = c_vectors2(13,:) >= moustache_level2;
+mask3 = c_vectors2(13,:) >= moustache_level;
 moustache_faces2 = uniquefaces(:,mask3);
 moustache_faces_cols2 = size(moustache_faces2, 2);       % Number of detected moustache faces (columns)   
 
@@ -178,7 +260,7 @@ layout = round(sqrt(moustache_faces_cols2));
 
 if layout == 0
     fprintfs("Zero faces detected for unqiue faces sample." + ...
-        " Moustache level is too high ( > %.2f )./n", max_level)
+        " Moustache level is too high ( > %.2f )./n", max_level2)
     return
 elseif layout^2 < moustache_faces_cols
     layout2 = layout + 1;
@@ -197,13 +279,13 @@ for i = 1: moustache_faces_cols2
     axis off
 end
 
-fprintf("There are %d unique faces detected with a moustache (Moustache Level = %d).\n", ...
-    moustache_faces_cols2, moustache_level2)
+fprintf("Unique faces: There are %d faces detected with a moustache (Moustache Level = %d).\n", ...
+    moustache_faces_cols2, moustache_level)
 
 
 % ACCURACY %
 
-% The accuracy of the detector is determined by using True Positive (TP), True Negative (TN),
+% As mentioned, the accuracy of the detector is determined by using True Positive (TP), True Negative (TN),
 % False Positive (FP), and False Negative (FN) values into the accuracy formula:
 
 % Accuracy = TP + TN / (TP + TN + FP + FN).     (EvidentlyAI, 2025)
@@ -224,21 +306,21 @@ FN = sum((groundtruth == 1) & (mask3 == 0));
 Accuracy = (TP + TN) / (TP + TN + FP + FN);
 fprintf("Unique Faces Sample Accuracy: %.2f%% \n", Accuracy * 100)
 
-% However, it is important to check the reliability of the computed accuracy as it may be undermined 
+% To reiterate, it is important to check the reliability of the computed accuracy as it may be undermined 
 % by the imbalance in the sample, where unique faces without a moustache significantly outnumber those
-% with a moustache. Since FP and FN = 0, the accuracy calculated is deemed reliable.
+% with a moustache. Since FP and FN = 0, the accuracy calculated is immediately deemed reliable.
 
-% Therefore, from a moustache level standpoint of 1800, the accuracy of the moustache detector on a sample of 
-% 35 unique faces is flawless at a 100% accuracy. *HOWEVER*, this accuracy is specific to the sample used, and
-% may not generalise to larger or differently composed datasets.
+% Therefore, from a moustache level standpoint of 1847.3, the accuracy of the moustache detector on a sample of 
+% 35 unique faces is flawless at a 100% accuracy. *HOWEVER*, again, this accuracy is specific to the sample used,
+% and may not generalise to larger or differently composed datasets.
 
 
 % Optimal moustache level: Accuracy vs. Moustache Level Plot
 
 % Calculating accuracy for moustache levels of 1 to the max level
-moustache_level_axis = 1:max_level;
+moustache_level_axis = 1:max_level2;
 
-for i = 1:max_level
+for i = 1:max_level2
     mask3 = c_vectors2(13,:) >= i;
     TP = sum((groundtruth == 1) & (mask3 == 1));
     TN = sum((groundtruth == 0) & (mask3 == 0)); 
@@ -258,19 +340,17 @@ upper_bound = moustache_coord_values(:, 9);        % Lowest moustache coordinate
 
 figure
 plot(moustache_level_axis, Accuracy * 100, 'HandleVisibility', 'off')
-title('Accuracy vs. Moustache Level')
+title('Unique Faces: Accuracy vs. Moustache Level')
 
-% Adding appropriate lines, axis titles and text
+% Plotting optimal boundary lines, adding axis titles and text
 hold on
-xline(moustache_level2, 'b:', 'DisplayName', ...
-    sprintf('Chosen Moustache Level (%.2f)', moustache_level2));
+xline(moustache_level, 'b:', 'DisplayName', ...
+    sprintf('Chosen Moustache Level (%.2f)', moustache_level));
 xline(lower_bound, '--r', 'DisplayName',  'Optimal Moustache Level Interval');
 xline(upper_bound, '--r', 'HandleVisibility',  'off');
-xlabel('Moustache Level'), ylabel('Accuracy (%)')
-text(upper_bound + 100, Accuracy(round(upper_bound)) * 100 - 8, ...
-    sprintf('100%% at Moustache Level\n ∈ [%.2f, %.2f]', ...
-lower_bound, upper_bound), 'Color', [0.5 0 0], 'FontSize', 7.5)
+xlabel('Moustache Level ϕ'), ylabel('Accuracy (%)')
+text(500, 50, sprintf('Maximum Accuracy: %.2f%% \nϕ ∈ [%.2f, %.2f]', max(Accuracy)*100, lower_bound, upper_bound), ...
+'Color', [0.5 0 0], 'FontSize', 8)
 
 legend('Show', 'Location', 'Northwest', 'FontSize', 8)
-
-xlim([0 max_level]), ylim([30 120])
+xlim([0 max_level2]), ylim([30 120])
